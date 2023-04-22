2503f28ba84790df19904b541a58561ba27c7b38
nikhil <109782243+ElegantFalcon@users.noreply.github.com>
2023-04-23 01:47:10 +0530

    V4

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { motion } from 'framer-motion' ;
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div></div>
   )
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -194,9 +194,9 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Coming Soon!",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "",
       tags: [
         {
           name: "react",
@@ -211,13 +211,14 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: carrent,
-      source_code_link: "https://github.com/",
+      
+      image: "",
+      source_code_link: "",
     },
     {
-      name: "Job IT",
+      name: "Coming Soon!",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "",
       tags: [
         {
           name: "react",
@@ -232,29 +233,29 @@ import {
           color: "pink-text-gradient",
         },
       ],
-      image: jobit,
-      source_code_link: "https://github.com/",
+      image: "",
+      source_code_link: "",
     },
     {
-      name: "Trip Guide",
+      name: "Coming Soon",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "",
       tags: [
         {
-          name: "nextjs",
+          name: "Django",
           color: "blue-text-gradient",
         },
         {
-          name: "supabase",
+          name: "CSS",
           color: "green-text-gradient",
         },
         {
-          name: "css",
+          name: "PostgreSql",
           color: "pink-text-gradient",
         },
       ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
+      image: "",
+      source_code_link: "/",
     },
   ];
   
